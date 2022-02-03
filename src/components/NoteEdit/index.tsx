0a1777d576eefe6f486de4cb6778283027bf9343
Beygs <boris.gilles@hotmail.fr>
2022-02-03 22:42:53 +0100

    input deactivation when there is nothing

--- src/components/NoteEdit/index.tsx
+++ src/components/NoteEdit/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { NoteProps } from "../../interfaces/interfaces";
-import Button from "../Button";
 import ContentEdit from "./ContentEdit";
 import TitleEdit from "./TitleEdit";
 
-const NoteEdit: React.FC<NoteProps> = ({ note, setNotes }) => (
-  <div className="NoteEdit">
-    <TitleEdit content={note?.title} setNotes={setNotes} noteKey={note?.key} />
-    <ContentEdit content={note?.content} setNotes={setNotes} noteKey={note?.key} />
-    <Button text="Enregistrer les changements" callback={() => console.log("Save")} />
-  </div>
-);
+const NoteEdit: React.FC<NoteProps> = ({ note, setNotes }) => {
+  if (!note) {
+    return (
+      <div className="NoteEdit">
+        <input disabled />
+        <textarea disabled />
+      </div>
+    );
+  }
+  return (
+    <div className="NoteEdit">
+      <TitleEdit content={note?.title} setNotes={setNotes} noteKey={note?.key} />
+      <ContentEdit content={note?.content} setNotes={setNotes} noteKey={note?.key} />
+    </div>
+  );
+};
 
 export default NoteEdit;
